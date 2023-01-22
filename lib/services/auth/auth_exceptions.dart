213c5f221addbc3346ac6910a2031d5d8363db50
void <amenu4cse20206@am.students.amrita.edu>
2023-01-22 15:10:27 +0530

    creating auth servives

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,14 @@
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+//generic exceptions
+class GenericAuthExceptions implements Exception {}
+
+class UserNotLoggedInAuthExceptions implements Exception {}
